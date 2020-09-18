0789467b80059bb1526e3f556d9ca534e0af19d9
daeun2001 <71498820+daeun2001@users.noreply.github.com>
2020-09-18 22:40:23 +0900

    first

---
+++ Practice02/src/Pratice02.java
@@ -0,0 +1,15 @@
+
+public class Pratice02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double percentage = 0.06;
+		double amount = 197.55;
+		double tax;
+		
+		tax = amount * percentage;
+	    
+		System.out.println("Sales tax is $" + (int)tax);
+	}
+
+}
